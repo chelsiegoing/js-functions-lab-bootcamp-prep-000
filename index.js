7f68d177ae5dfbc2a8124c83bfa32eb974f3682f
lydia <lydiahamil@yahoo.com>
2017-11-13 00:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(string) {
+  return 'Happy holidays!'
+}
+function happyHolidaysTo(name) {
+  console.log('Happy holidays')
+  return `Happy holidays, ${name}!`
+}
+function holidayCountdown(days) {
+  console.log('Days until')
+  return `It\'s ${days} ${holiday}`
+}
